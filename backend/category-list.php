18f9739a76c539b81d2f6d84edad976ac4d1d9a5
hoangtho <newcentury_2030@yahoo.com>
2015-11-26 10:44:27 +0700

    fix conflict

--- backend/category-list.php
+++ backend/category-list.php
@@ -86,39 +86,9 @@
 		</div>
 	</div>
 	
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mymodalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close form-close" data-dismiss="modal">
-					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
-				</button>
-				<h4 class="modal-title" id="myModalLabel">Add New Category</h4>
-			</div>
-            		<div class="modal-body">
-				<form action="category-action.php" method="POST" id="cate-form" class="form-horizontal">
-					<div class="form-group">
-						<label for="cate-name" class="col-md-3 control-label">Name: </label>
-						<div class="col-md-9">
-							<input class="form-control" type="text" name="cate-name" id="cate-name">
-						</div>
-					</div>	
-					<div class="form-group">
-						<div class="col-md-9 col-md-offset-3">
-							
-							<button type="button" class="btn btn-default form-close" data-dismiss="modal">Close</button>
-							<input class="btn btn-info btn-danger" type="submit" name="edit" id="btn_submit" value="Save">	
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+	
 	
 
-<?php
-  if(isset($_POST['']))
-?>
 	
 <?php 
 	include ('templates/footer.php');
@@ -174,17 +144,6 @@
 		$("#btn_submit").attr("name","submit_add");
 	});
 	
-<<<<<<< HEAD
-	
-=======
-	$(document).on('click','#editCate',function(evt){
-		
-		$("#mymodalLabel").text("Category Edit Form");
-		$("#userID").show();
-		$("#userID").val($(this).attr("data-id"));
-		$("#cate-name").val($(this).attr("data-name"));
-		$("#btn_submit").attr("name","edit");
-	});
->>>>>>> 5f67f5fecfd93a27a64a3d283f8f7c9b6ac3e73d
+
 	
 </script>
